927d6b5689d806f7954646872e79df357f98d844
@pedro.velasquez1 <pedro.velasquez1@holcim.com>
2026-08-27 11:09:14 -0500

    update el camino

--- src/app/components/carousel/carousel.ts
+++ src/app/components/carousel/carousel.ts
@@ -23,7 +23,7 @@ export class Carousel implements OnDestroy {
   constructor(private cdr: ChangeDetectorRef) {}
 
   images: CarouselImage[] = [
-    { src: 'Eventos/DiaUnidad.jpeg', startDate: '2026-08-24', endDate: '2026-09-05' }
+    { src: 'Eventos/DiaUnidad.jpeg', startDate: '2026-08-24', endDate: '2026-09-05' },
     { src: 'Eventos/Elcamino1.jpeg', startDate: '2026-08-27', endDate: '2026-09-05' },
     { src: 'Eventos/EncuentroServidores.jpeg', startDate: '2026-08-09', endDate: '2026-09-13' },
     { src: 'Eventos/VideoEncuentroServidores.mp4', mediaType: 'video', startDate: '2026-08-09', endDate: '2026-09-13' },
